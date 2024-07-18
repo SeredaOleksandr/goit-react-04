f99ae44902cd9aad06ef8dbe3eace1d645c5ee5d
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-07-18 12:54:56 +0300

    add Memo

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,40 +1,67 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
-import ArticleList from '../ArticleList/ArticleList';
-import { fetchArticlesWithTopic } from '../articles-api';
-import SearchForm from '../SearchForm/SearchForm';
-import Loader from '../Loader/Loader';
-import Error from '../Error/Error';
+// import ArticleList from '../ArticleList/ArticleList';
+// import { fetchArticlesWithTopic } from '../articles-api';
+// import SearchForm from '../SearchForm/SearchForm';
+// import Loader from '../Loader/Loader';
+// import Error from '../Error/Error';
 
-function App() {
-  const [articles, setArticles] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
+// function App() {
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(false);
+//   const [error, setError] = useState(false);
 
-  const handleSearch = async topic => {
-    try {
-      setArticles([]);
-      setError(false);
-      setLoading(true);
-      const data = await fetchArticlesWithTopic(topic);
-      setArticles(data);
-    } catch (error) {
-      setError(true);
-    } finally {
-      setLoading(false);
-    }
-  };
+//   const handleSearch = async topic => {
+//     try {
+//       setArticles([]);
+//       setError(false);
+//       setLoading(true);
+//       const data = await fetchArticlesWithTopic(topic);
+//       setArticles(data);
+//     } catch (error) {
+//       setError(true);
+//     } finally {
+//       setLoading(false);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <SearchForm onSearch={handleSearch} />
+//       <h1>Latest articles</h1>
+//       {loading && <Loader />}
+//       {error && <Error />}
+//       {articles.length > 0 && <ArticleList items={articles} />}
+//     </div>
+//   );
+// }
+
+// export default App;
+import { useMemo, useState } from 'react';
+
+const App = () => {
+  const [planets, setPlanets] = useState(['Earth', 'Mars', 'Jupiter', 'Venus']);
+  const [query, setQuery] = useState('');
+  const [clicks, setClicks] = useState(0);
+
+  const filteredPlanets = useMemo(
+    () => planets.filter(planet => planet.includes(query)),
+    [planets, query]
+  );
 
   return (
-    <div>
-      <SearchForm onSearch={handleSearch} />
-      <h1>Latest articles</h1>
-      {loading && <Loader />}
-      {error && <Error />}
-      {articles.length > 0 && <ArticleList items={articles} />}
-    </div>
+    <>
+      <button onClick={() => setClicks(clicks + 1)}>
+        Number of clicks: {clicks}
+      </button>
+      <ul>
+        {filteredPlanets.map(planet => (
+          <li key={planet}>{planet}</li>
+        ))}
+      </ul>
+    </>
   );
-}
+};
 
 export default App;
